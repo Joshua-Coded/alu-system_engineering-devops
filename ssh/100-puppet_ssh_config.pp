02b1567741b52df4c2da6b17379a20d1a4b4219e
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-19 15:00:54 +0200

    ssh connection using private key

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
+# This is a Puppet configuration file. It defines how Puppet will manage the systems in your infrastructure.
+
 file {
-  "sshd_config":
+  'sshd_config':
     ensure => present,
-    path => 'etc/ssh/ssh_config',
-    content => <<EOF
-
-
-   Host *
-    IdentityFile ~/.ssh/school
-    PaswordAuthentication no
-   EOF
+    path => '/etc/ssh/ssh_config',
+    content => <<EOFF
+Host *
+  IdentityFile ~/.ssh/school
+  PasswordAuthentication no
+EOFF
 }
